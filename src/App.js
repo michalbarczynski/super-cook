9ac6810728af93257fefa6f1a7faf0d6b3e26589
Michał Barczyński <michal.barczynski97@gmail.com>
2024-08-04 13:48:32 +0200

    finish app ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
